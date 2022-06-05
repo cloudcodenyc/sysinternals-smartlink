26b1a16eb93e603bce1bc79d56ccf70c7000de19
cloudcodenyc <98148819+cloudcodenyc@users.noreply.github.com>
2022-06-04 23:54:27 -0400

    Add Powershell ExecutionPolicy
    Powershell ExecutionPolicy RemoteSigned

--- sysinternals-filelink_sym.ps1
+++ sysinternals-filelink_sym.ps1
@@ -28,6 +28,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION       #
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.             #
 ###############################################################################
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
 
 	$STYPE="symlink"
 	$SPATH=".\sysinternals-$STYPE"
